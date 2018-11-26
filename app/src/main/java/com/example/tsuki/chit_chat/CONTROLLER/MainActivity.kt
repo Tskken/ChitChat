a2adcb8c3615530ad58cd01a6f623c8904616d87
Alice Easter <alice.easterdevelops@gmail.com>
2018-11-26 17:21:24 -0500

    initial message format defined

--- app/src/main/java/com/example/tsuki/chit_chat/CONTROLLER/MainActivity.kt
+++ app/src/main/java/com/example/tsuki/chit_chat/CONTROLLER/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import com.example.tsuki.chit_chat.R
 import com.example.tsuki.chit_chat.UTIL.CCApiTask
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,7 +30,30 @@ class MainActivity : AppCompatActivity() {
 
         textView.text = data
 
-        // TODO: Process feed data
+        val jsonData = JSONObject(data)
+
+        val count = jsonData.getInt("count")
+        val date = jsonData.getString("date")
+        val messages = jsonData.getJSONArray("messages")
+
+        /*
+        message format:
+
+        {
+          "_id" : "UUID",
+          "client" : "email@champlain.edu",
+          "date" : "datestring",
+          "dislikes" : 0,
+          "ip" : "255.255.255.255",
+          "likes" : 0,
+          "loc" : [
+            null,
+            null
+          ],
+          "message" : "lorem ipsum dolor sit amet"
+        }
+
+        */
     }
 
     private fun onFeedRequestFailure() {
