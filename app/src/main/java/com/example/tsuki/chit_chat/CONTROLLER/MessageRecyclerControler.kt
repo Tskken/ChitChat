4aaed26ff4044adf7c56f72f3a5a2c4a0f43ebab
Tsukinai <dylan.blanchard@mymail.champlain.edu>
2018-11-30 08:32:42 -0500

    updated for recycler
    updated code to include basic recycler implementation

--- app/src/main/java/com/example/tsuki/chit_chat/CONTROLLER/MessageRecyclerControler.kt
+++ app/src/main/java/com/example/tsuki/chit_chat/CONTROLLER/MessageRecyclerControler.kt
@@ -2,18 +2,31 @@ package com.example.tsuki.chit_chat.CONTROLLER
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.tsuki.chit_chat.MODEL.MessageManager
+import android.support.v7.widget.RecyclerView
+import android.widget.Button
+import android.widget.TextView
+import com.example.tsuki.chit_chat.MODEL.Message
+import com.example.tsuki.chit_chat.R
 import com.example.tsuki.chit_chat.UTIL.CCApiTask
-import com.example.tsuki.chit_chat.UTIL.EncoderDecoder
 
 class MessageRecyclerControler : Fragment() {
     private var mMessageManager: MessageManager? = null
+    private var mRecyclerView: RecyclerView? = null
+    private var mAdapter: MessageAdapter? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.recycler_view, container, false)
+
         // create api task to get initial data
         val apiTask = CCApiTask(
                 sServerURL,
@@ -24,14 +37,80 @@ class MessageRecyclerControler : Fragment() {
 
         // execute api call
         apiTask.execute()
+
+        // Initialize RecyclerView //
+        mRecyclerView = view.findViewById(R.id.recycler_view) as RecyclerView
+        mRecyclerView!!.layoutManager = LinearLayoutManager(activity)
+
+
+
+        updateUI()
+
+        return view
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    private inner class MessageView(inflater: LayoutInflater, parent: ViewGroup) :
+        RecyclerView.ViewHolder(inflater.inflate(R.layout.recycler_view, parent, false)
+    ) {
+        private var mUName = parent.findViewById(R.id.user_name) as TextView
+        private var mMessageText = parent.findViewById(R.id.message) as TextView
+        private var mLikeButton = parent.findViewById(R.id.like_button) as Button
+        private var mDislikeButton = parent.findViewById(R.id.dislike_button) as Button
+
+        init {
+            mLikeButton.setOnClickListener {
+                //TODO: Have like post to server
+            }
+            mDislikeButton.setOnClickListener {
+                //TODO: have dislike post to server
+            }
+        }
+
+        fun bind(message: Message) {
+           var uName = message.client.split("@", ".").joinToString(
+                   separator = " ",
+                   prefix = "",
+                   postfix = "",
+                   limit = 2
+           )
+            mUName.text = uName
+            mMessageText.text = message.message
+            mLikeButton.text = message.likes.toString()
+            mDislikeButton.text = message.dislikes.toString()
+        }
     }
+
+    private inner class MessageAdapter : RecyclerView.Adapter<MessageView>() {
+        override fun getItemCount(): Int {
+            return mMessageManager!!.messages.size
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageView {
+            val layoutInflater = LayoutInflater.from(activity)
+            return MessageView(layoutInflater, parent)
+        }
+
+        override fun onBindViewHolder(holder: MessageView, position: Int) {
+            holder.bind(mMessageManager!!.messages[position])
+        }
+    }
+
+    // Updated UI //
+    private fun updateUI() {
+        // Check if an adapter exists //
+        // -Create a new one if non exists
+        if ( mAdapter == null) {
+            this.mAdapter = MessageAdapter()
+            mRecyclerView!!.adapter = mAdapter
+        } else {
+            // Update UI //
+            activity!!.runOnUiThread { mAdapter!!.notifyDataSetChanged() }
+        }
+    }
+
 
     private fun onFeedData(data: String) {
-        this.mMessageManager = EncoderDecoder.Decode(data)
+        this.mMessageManager = MessageManager.decode(data)
     }
 
     private fun likeMessage(messageId: String) {
--- app/src/main/java/com/example/tsuki/chit_chat/MODEL/MessageManager.kt
+++ app/src/main/java/com/example/tsuki/chit_chat/MODEL/MessageManager.kt
@@ -1,9 +1,16 @@
 package com.example.tsuki.chit_chat.MODEL
 
+import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 
 class MessageManager(
         @SerializedName("count") var count: Int = 0,
         @SerializedName("date") var date: String = "",
         @SerializedName("messages") var messages: Array<Message> = arrayOf()
-)
+) {
+    companion object {
+        fun decode(json: String): MessageManager {
+            return Gson().fromJson<MessageManager>(json, MessageManager::class.java)
+        }
+    }
+}
